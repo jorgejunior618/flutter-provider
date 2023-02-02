0bea1bc562637ac88b86d9c9fcc2e4da37585b39
jorgejunior618 <jorgejunior618@gmail.com>
2023-02-02 14:19:29 -0300

    Criação do gerador de estados

--- lib/provider/index.dart
+++ lib/provider/index.dart
@@ -1,3 +1,20 @@
+import 'package:client_control/models/client_types/client_type.dart';
+import 'package:client_control/models/clients/client.dart';
 import 'package:client_control/models/clients/client_list.dart';
+import 'package:flutter/material.dart';
 
-createNotifierProvider(context) => ClientList(clients: []);
+List<ClientType> types = [
+  ClientType(name: 'Platinum', icon: Icons.credit_card),
+  ClientType(name: 'Golden', icon: Icons.card_membership),
+  ClientType(name: 'Titanium', icon: Icons.credit_score),
+  ClientType(name: 'Diamond', icon: Icons.diamond),
+];
+
+ClientList createNotifierProvider(BuildContext context) {
+  return ClientList(clients: [
+    // Client(name: 'Geraldo', email: 'leo@email.com', type: types[0]),
+    // Client(name: 'Paulo', email: 'leo@email.com', type: types[1]),
+    Client(name: 'Caio', email: 'leo@email.com', type: types[2]),
+    // Client(name: 'Ruan', email: 'ruan@email.com', type: types[3]),
+  ]);
+}
