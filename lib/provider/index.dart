7ab8ba6c02542275d1a31abe414b23a21bc74525
jorgejunior618 <jorgejunior618@gmail.com>
2023-02-02 14:51:27 -0300

    Limpeza de testes

--- lib/provider/index.dart
+++ lib/provider/index.dart
@@ -1,5 +1,4 @@
 import 'package:client_control/models/client_types/client_type.dart';
-import 'package:client_control/models/clients/client.dart';
 import 'package:client_control/models/clients/client_list.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +10,5 @@ List<ClientType> types = [
 ];
 
 ClientList createNotifierProvider(BuildContext context) {
-  return ClientList(clients: [
-    // Client(name: 'Geraldo', email: 'leo@email.com', type: types[0]),
-    // Client(name: 'Paulo', email: 'leo@email.com', type: types[1]),
-    Client(name: 'Caio', email: 'leo@email.com', type: types[2]),
-    // Client(name: 'Ruan', email: 'ruan@email.com', type: types[3]),
-  ]);
+  return ClientList([]);
 }
