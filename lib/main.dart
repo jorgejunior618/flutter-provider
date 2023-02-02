19cf54fd81ea84c5d1e1feb0d799b091c02b88d0
jorgejunior618 <jorgejunior618@gmail.com>
2023-02-02 10:54:06 -0300

    Refatoração de arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
-import 'pages/clients_page.dart';
+import 'pages/client_page/clients_page.dart';
+import 'package:client_control/pages/client_types_page/client_types_page.dart';
 
 void main() {
   runApp(const MyApp());
